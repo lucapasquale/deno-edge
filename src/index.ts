97117b9370b6716697bb14abf1cc7c2df9928e6c
Luca Pasquale <lucapasquale.094@gmail.com>
2022-12-15 21:29:07 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+
+serve((req) => {
+  console.log(req);
+
+  return new Response("Hello World!", {
+    headers: { "content-type": "text/plain" },
+  });
+});
